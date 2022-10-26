195fb26054e316f9365fcaecdc525aa5b538a613
Caroline Mimeault <carolinemimeault@gmail.com>
2022-10-25 21:35:52 -0400

    working on the widget

--- flight-widget-api/server/server.js
+++ flight-widget-api/server/server.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.get("/flights", function (req, res) {
     const options = {
         method: 'GET',
-        url: 'https://toronto-pearson-airport.p.rapidapi.com/arrivals/carousel/9',
+        url: 'https://toronto-pearson-airport.p.rapidapi.com/departures',
         headers: {
             'X-RapidAPI-Key': process.env.RAPID_API_KEY,
             'X-RapidAPI-Host': 'toronto-pearson-airport.p.rapidapi.com'
@@ -17,10 +17,11 @@ app.get("/flights", function (req, res) {
     }
     axios.request(options).then(function (response) {
         console.log(response.data)
-        res.json(response.data)
+        res.json(response.data.slice(0, 6))
     }).catch(function (error) {
         console.error(error)
     })
 })
 
 app.listen(PORT, () => console.log("running on PORT" + PORT))
+// run server and then open browser to localhost:8000/flights
